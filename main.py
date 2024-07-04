beebaac3319c0f0bbab0c3f04e983d365ccc06bb
Yo-ru <yoru@its.moe>
2024-07-04 16:45:42 -0500

    init: database

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 import os
+import databases
 
-from quart import Quart, render_template, request
+from quart import Quart
 
 import settings
+
+from log import log, Ansi
 
 app = Quart(__name__)
 app.secret_key = os.urandom(32)
 app.permanent_session_lifetime = 86400 # 1 day
-
-@app.before_serving
-async def before_serving():
-    print('osu!stream backend is starting...')
 
 # register blueprints
 from blueprints.admin import admin
@@ -21,6 +20,27 @@ app.register_blueprint(score, url_prefix='/score')
 
 from blueprints.auth import auth
 app.register_blueprint(auth, url_prefix='/auth')
+
+@app.before_serving
+async def before_serving():
+    log('=== osu!stream backend ===', Ansi.LMAGENTA)
+    
+    # check if the database is accessible
+    app.db = databases.Database(settings.DB_DSN)
+    log('Connecting to the database...', col=Ansi.LCYAN, end=' ')
+    try:
+        await app.db.connect()
+        await app.db.disconnect()
+        log('SUCCESS', Ansi.LGREEN, timestamp=False)
+    except Exception as e:
+        log(f'FAILED', Ansi.LRED, timestamp=False)
+        log(f'Error: {Ansi.RESET!r}{e}', Ansi.LRED)
+        log('==========================', Ansi.LMAGENTA)
+        os._exit(1)
+    log('==========================', Ansi.LMAGENTA)
+        
+        
+    
 
 if __name__ == '__main__':
     app.run(
